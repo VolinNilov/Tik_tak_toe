178b02ded8eca9c1c0c7560b9603163bcfda7851
Vladimir Blinov <70836867+Hero228-ux@users.noreply.github.com>
2022-08-02 11:20:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from telebot import types
 from DB import DB
 import mysql.connector
 import json
+import random
 
 bot = telebot.TeleBot(config.TOKEN)
 database = DB(config.mysql)
@@ -21,8 +22,8 @@ def get_user(message):
     if (data):
         return {"id": data[0][0], "name": data[0][1], "status": data[0][2], "settings": json.loads(data[0][3])}
     else:
-        database.insert('users', ['id', 'name', 'status', 'settings'], [[message.chat.id, message.chat.first_name, 'menu', '{\"subscribe\": [], \"commands\": []}']])
-        return {"id": message.chat.id, "name": message.chat.first_name, "status": 'menu', "settings": {"subscribe": [], "commands": []}}
+        database.insert('users', ['id', 'name', 'status', 'settings'], [[message.chat.id, message.chat.first_name, 'menu', '{\"win\": [], \"lost\": []}']])
+        return {"id": message.chat.id, "name": message.chat.first_name, "status": 'menu', "settings": {"win": [], "lost": []}}
 
 def log(message, user):
     query = "INSERT INTO log (text) VALUES (%s)"
@@ -45,12 +46,6 @@ def markups(buttons):
 
 def menu_markups(user):
     answer = markups(["Поиск противника🔍", "Настройки⚙️", "Информацияℹ️"])
-    b = []
-    for i in user["settings"]["commands"]:
-        b.append(types.KeyboardButton(i))
-    if b:
-        answer.add(*b)
-    return answer
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
@@ -65,5 +60,69 @@ def start_message(message):
     bot.send_message(message.chat.id,"Перезаряжаю!!!!!!!!!!", reply_markup=menu_markups(user))
     log(message, user)
     user_update(user, "menu")
+
+class MessageHandler:
+    def menu(bot, message, user):
+        if "НАСТРОЙКИ" in message.text:
+            # return MessageHandler.Settings.to_main(bot, message, user)
+            return True
+        
+        if "ПОИСК ПРОТИВНИКА":
+            return MessageHandler.found(bot, message, user)
+
+    def to_menu(bot, message, user):
+        bot.send_message(user["id"], "Хорошего дня", reply_markup=menu_markups(user))
+        user_update(user, status="menu")
+        return True
+    
+    # class Settings:
+    #     def main(bot, maessage, user):
+    
+    def found(bot, message, user):
+        bot.send_message(user["id"], "Поиск соперника", reply_markup=menu_markups(user))        
+        user_update(user, status="found")
+        if 
+        return MessageHandler.Game.menu(bot, message, user)
+    
+    class Game:
+        def menu(bot, message, user):
+            user_update(user, status="game")
+            bot.send_message(user["id"], "Игра найденна", reply_markup=markups("Стоп"))
+            if "СТОП" in message.text:
+                bot.send_message(user["id"], "Вы уверенны?", reply_markups=markups("Да", "Отмена"))
+
+                if "ДА" in message.text:
+                    bot.send_message(user["id"], "Выход из матча", reply_markups=menu_markups(user))
+                    user_update(user, status="menu")
+                    return True
+
+                if "Отмена" in message.text:
+                    return True
+                
+        def to_menu(bot, message, user):
+            bot.send_message(user["id"], "Хорошего дня", reply_markup=menu_markups(user))
+            user_update(user, status="game_menu")
+            return True
+    
+
+@bot.message_handler(content_types=["text"])
+def handle_text(message):
+    print(f"{message.chat.id} {message.chat.first_name} |{message.text}|")
+    message.text = message.text.strip().replace("  ", " ").replace("\t\t", "\t")
+    user = get_user(message)
+    message.text = message.text.upper()
+    log(message, user)
+    action = {
+        "menu": MessageHandler.menu,
+        # "settings": MessageHandler.Settings.main,
+        "found": MessageHandler.found,
+        "game_menu": MessageHandler.Game.menu
+    }
+    if action.get(user["status"]):
+        if not action[user["status"]](bot, message, user):
+            bot.send_message(user["id"], "Не понял!")
+    else:
+        bot.send_message(user["id"], f"Статус {user['status']} не найден!")
+    return
 
 bot.polling()
